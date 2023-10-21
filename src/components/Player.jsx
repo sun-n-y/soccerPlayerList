f1337d8cc7b7ecc50a12a19d9168a30f9869eec4
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-21 15:49:23 -0400

    app complete

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,10 @@
 const Player = ({ number, name, image }) => {
   return (
     <div className="person">
-      <img src={image} alt={name} />/
+      <img src={image} alt={name} />
       <div>
         <h4>{name}</h4>
-        <p>jersey number:{number}</p>
+        <p>jersey number: {number}</p>
       </div>
     </div>
   );
