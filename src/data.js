c650993a3542c4f15bf4b3204aeb778d135b28f9
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-21 14:22:52 -0400

    add data

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const data = [
+  {
+    id: 1,
+    name: 'lionel messi',
+    number: 10,
+    image:
+      'https://images.pexels.com/photos/14899674/pexels-photo-14899674.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: 2,
+    name: 'cristiano ronaldo',
+    number: 7,
+    image:
+      'https://images.pexels.com/photos/11565658/pexels-photo-11565658.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: 3,
+    name: 'harry kane',
+    number: 9,
+    image:
+      'https://images.pexels.com/photos/7710514/pexels-photo-7710514.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: 4,
+    name: 'david silva',
+    number: 10,
+    image:
+      'https://images.pexels.com/photos/54097/spain-flag-flutter-spanish-54097.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: 5,
+    name: 'ronaldo',
+    number: 9,
+    image:
+      'https://images.pexels.com/photos/10354052/pexels-photo-10354052.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+];
